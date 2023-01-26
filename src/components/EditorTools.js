dc2a06f2688ffd1bd647b1b75c894a9fdce462c9
Marcus-Rise <ilykonst95@gmail.com>
2023-01-26 14:32:45 +0400

    inline tools

--- src/components/EditorTools.js
+++ src/components/EditorTools.js
@@ -5,33 +5,17 @@ import List from '@editorjs/list';
 import Delimiter from '@editorjs/delimiter';
 import Image from '@editorjs/image';
 import Table from '@editorjs/table';
-import createGenericInlineTool, {
-  ItalicInlineTool,
-  UnderlineInlineTool,
-} from 'editorjs-inline-tool';
+import {UnderlineInlineTool,} from 'editorjs-inline-tool';
 
 
 export const EDITOR_TOOLS = {
+  underline: UnderlineInlineTool,
   code: Code,
   header: Header,
   paragraph: {
     class: Paragraph,
     inlineToolbar: true,
   },
-  bold: {
-    class: createGenericInlineTool({
-      sanitize: {
-        strong: {},
-      },
-      shortcut: 'CMD+B',
-      tagName: 'STRONG',
-      toolboxIcon:
-        '<svg class="icon icon--bold" width="12px" height="14px"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#bold"></use></svg>',
-    }),
-  },
-  // or use a pre-defined tool instead
-  italic: ItalicInlineTool,
-  underline: UnderlineInlineTool,
   list: {
     class: List,
     inlineToolbar: true,
