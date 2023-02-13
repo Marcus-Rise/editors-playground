8c83a9b4b4e7871d50e194c1ce8ec993e0b65a53
Marcus-Rise <ilykonst95@gmail.com>
2023-02-13 09:12:59 +0400

    log

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,34 @@
-import React, {FC} from 'react';
+import React, {ComponentProps, FC, useCallback} from 'react';
 import {Editor, EditorContextProvider, EditorToolbar} from "./Editor";
 import styled, {ThemeProvider} from "styled-components";
 import {LIGHT_THEME} from "@admiral-ds/react-ui";
 import {initialData} from "./initial.data";
+import {serialize} from "./Editor/utils/serialize.helper";
+import {Descendant} from "slate";
 
 const Main = styled.main`
   padding: 1rem;
 `;
 
-const App: FC = () =>
-  (
+const App: FC = () => {
+  const sendData: ComponentProps<typeof EditorContextProvider>["onChange"] = useCallback((dto: Descendant[]) => {
+    console.log("editor data", dto);
+
+    const html = dto.map((node) => serialize(node)).join('\n');
+
+    console.log("serialized data", html);
+  }, []);
+
+  return (
     <ThemeProvider theme={LIGHT_THEME}>
       <Main>
-        <EditorContextProvider value={initialData} onChange={console.log}>
+        <EditorContextProvider value={initialData} onChange={sendData}>
           <EditorToolbar/>
           <Editor/>
         </EditorContextProvider>
       </Main>
     </ThemeProvider>
   );
+};
 
 export default App;
